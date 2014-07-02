555e736fef3a06ea020c33a9f1ce16c36b5793e3
Jean-Charles Campagne <jc.campagne@gmail.com>
2014-07-02 11:57:57 +0100

    Initial commit with board and test

---
+++ FS2048/Game.fs
@@ -0,0 +1,35 @@
+﻿namespace FS2048
+
+
+module Game =
+    
+    [<Measure>]
+    type C
+
+    [<Measure>]
+    type R
+
+    [<Measure>]
+    type V
+
+    type Col = int<C>
+    type Row = int<R>
+    type Cval = int<V>
+
+    type Board = int<V> [] []
+
+    let (zeroBoard : Board) =
+         [| [| 0<V>; 0<V>; 0<V>; 0<V>|]
+            [| 0<V>; 0<V>; 0<V>; 0<V>|]
+            [| 0<V>; 0<V>; 0<V>; 0<V>|]
+            [| 0<V>; 0<V>; 0<V>; 0<V>|]
+         |]
+
+    let int_of_row (row:Row) = row / 1<R>
+    let int_of_col (col:Col) = col / 1<C>
+
+    let set (b:Board) (row:Row) (col:Col) (value:Cval) =
+        let i = int_of_row(row)
+        let j = int_of_col(col)
+        b.[i].[j] <- value
+    
---
+++ FS2048/Test.fs
@@ -0,0 +1,30 @@
+﻿namespace FS2048
+open System
+open NUnit.Framework
+
+open Game
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member x.``Init zero board``() =
+        let result = Game.zeroBoard
+        let (expected : int<Game.V> [][]) =
+                       [| [| 0<Game.V>; 0<Game.V>; 0<Game.V>; 0<Game.V>|]; // semicolon or not
+                          [| 0<Game.V>; 0<Game.V>; 0<Game.V>; 0<Game.V>|]
+                          [| 0<Game.V>; 0<Game.V>; 0<Game.V>; 0<Game.V>|]
+                          [| 0<V>;      0<V>;      0<V>;      0<V>     |] // V or Game.V works
+                       |]
+        Assert.AreEqual (result, expected)
+    
+    [<Test>]
+    member x.``Set test``() =
+        let b = Game.zeroBoard
+        let _ = Game.set b 0<R> 0<C> 2<V>
+        let result = b.[0].[0]
+        Assert.IsTrue ((result = 2<V>):bool)
+//      Assert.IsTrue ((result = 2):bool)  // this does not compile… as expected
+        Assert.AreEqual (result, 2<V>)
+        Assert.AreEqual (result, 2) // works as well…?
+

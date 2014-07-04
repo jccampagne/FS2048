de7c67602df47ce77110421a75bdbe681e49e810
Jean-Charles Campagne <jc.campagne@gmail.com>
2014-07-04 16:02:13 +0100

    Split play code from general code

---
+++ ConsolePlay/ConsolePlay.fs
@@ -0,0 +1,26 @@
+﻿namespace FS2048
+
+module ConsolePlay =
+    let keyToMove k =
+        match k with
+              System.ConsoleKey.LeftArrow  -> Some FS2048.Game.Left
+            | System.ConsoleKey.DownArrow  -> Some FS2048.Game.Down
+            | System.ConsoleKey.UpArrow    -> Some FS2048.Game.Up
+            | System.ConsoleKey.RightArrow -> Some FS2048.Game.Right
+            | _ -> None
+            
+
+    let displayGame (g:Game.State) =
+        let b = g.board
+        for i in b do
+            printf "%A\n" i
+        printfn "score = %A" g.score
+        printfn "---------------------------"
+
+    let rec getMove g =
+        displayGame g
+        let keyInfo = System.Console.ReadKey ()
+        printfn " key  = %A" keyInfo.Key
+        match keyToMove keyInfo.Key with
+            | Some k -> k
+            | None -> getMove g
--- ConsolePlay/Program.fs
+++ ConsolePlay/Program.fs
@@ -1,34 +1,7 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-open FS2048
-
-
-let keyToMove k =
-    match k with
-          System.ConsoleKey.LeftArrow  -> Some FS2048.Game.Left
-        | System.ConsoleKey.DownArrow  -> Some FS2048.Game.Down
-        | System.ConsoleKey.UpArrow    -> Some FS2048.Game.Up
-        | System.ConsoleKey.RightArrow -> Some FS2048.Game.Right
-        | _ -> None
-        
-
-let displayGame (g:Game.State) =
-    let b = g.board
-    for i in b do
-        printf "%A\n" i
-    printfn "score = %A" g.score
-    printfn "---------------------------"
-
-let rec getMove g =
-    displayGame g
-    let keyInfo = System.Console.ReadKey ()
-    printfn " key  = %A" keyInfo.Key
-    match keyToMove keyInfo.Key with
-        | Some k -> k
-        | None -> getMove g
+﻿open FS2048
 
 let rec loop g =
-    let move = getMove g
+    let move = ConsolePlay.getMove g
     match Game.play g move with
     | Game.GameOver h -> ()
     | Game.GameContinue h -> loop h
